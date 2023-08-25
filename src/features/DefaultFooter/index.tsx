a6f54999c3586ef84140b832eb6f5049bc98344a
aaron phillips <aaronp48@gmail.com>
2023-08-25 10:00:52 -0700

    refactor base styles to be feature based

--- src/features/DefaultFooter/index.tsx
+++ src/features/DefaultFooter/index.tsx
@@ -1,40 +1,7 @@
 "use client";
-import {
-  Avatar,
-  createStyles,
-  Container,
-  Group,
-  Anchor,
-  rem,
-} from "@mantine/core";
+import { Container, Group, Anchor } from "@mantine/core";
 import { IconCurrencyTenge } from "@tabler/icons-react";
-
-const useStyles = createStyles((theme) => ({
-  footer: {
-    marginTop: rem(120),
-    borderTop: `${rem(1)} solid ${
-      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
-    }`,
-  },
-
-  inner: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingTop: theme.spacing.xl,
-    paddingBottom: theme.spacing.xl,
-
-    [theme.fn.smallerThan("xs")]: {
-      flexDirection: "column",
-    },
-  },
-
-  links: {
-    [theme.fn.smallerThan("xs")]: {
-      marginTop: theme.spacing.md,
-    },
-  },
-}));
+import { useStyles } from "./styles";
 
 const links = [
   {
